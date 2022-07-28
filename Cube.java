14cf06d96d21b3aab9b137cacf426ad2feebea6f
harshitjain0786 <109657173+harshitjain0786@users.noreply.github.com>
2022-07-29 00:57:18 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,25 @@
+//Write a  Java program  to display the cube of the number upto given  integer.
+
+import java.util.Scanner;
+ class Cube
+ {                                 
+
+   public static void main(String[] args)
+
+{
+    int i,No,Cube;
+    System.out.println("Enter a No :- ");
+    Scanner scan = new Scanner(System.in);     
+		    No = scan.nextInt();
+
+     for(i=1;i<=No;i++)                      
+     {
+         
+         Cube = i*i*i;                		
+         System.out.println("Num is " +i+ " Cube of Num :-" +i+" is : "  +Cube );
+         // System.out.println("\n");
+
+    }
+
+ }
+}
---
+++ Triangle.java
@@ -0,0 +1,24 @@
+//THE PROGRAMME OF INCREASING A NUMBERS 
+
+import java.util.Scanner;
+class Triangle 						
+{
+	public static void main(String[] args)
+	{
+		int i,j,num;
+		System.out.println("enter num. :- ");
+		Scanner scan = new Scanner(System.in);	
+		num = scan.nextInt();
+
+		for(i=1;i<=num;i++) 			
+		{
+			for (j=2;j=>i;j++) 
+            			
+				System.out.println(j);
+			
+			System.out.println("");
+
+		}
+
+	}
+}
